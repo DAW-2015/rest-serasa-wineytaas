057c4a0a5c99885e4840915620e080058bb94c76
Wisney <wisneytaas@gmail.com>
2015-09-29 10:42:16 -0300

    Banco De Dados
    Editei os parâmetros para o meu banco de dados

--- connection.php
+++ connection.php
@@ -2,14 +2,13 @@
 
 class Connection
 {
-  public static $database = "serasa";
-  public static $address = "127.0.0.1";
-  public static $user = "root";
-  public static $password = "";
+  public static $database = "daw-aluno2";
+  public static $address = "150.164.102.160";
+  public static $user = "daw-aluno2";
+  public static $password = "wisney";
 
   public static function getConnection() {
     $connection = mysqli_connect(Connection::$address, Connection::$user, Connection::$password, Connection::$database);
-
     return $connection;
   }
 }
